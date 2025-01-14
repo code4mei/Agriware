759b4007553ce2bb9162dfef2d8471aaff514b46
Rabindra Mishra <sambhu311275@gmail.com>
2025-01-15 01:51:31 +0530

    Ready with all rendered HTML Page.

--- ecommerce/ecommerceapp/views.py
+++ ecommerce/ecommerceapp/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,"index.html")
+
+def contact(request):
+    return render(request,"contact.html")
+
+def about(request):
+    return render(request,"about.html")
+
+def prediction(request):
+    return render(request,"prediction.html")
